8560e77c99025012c7e1cdeed32a9df661349b0b
Bob <bob+git@vulpin.com>
2018-06-17 02:44:56 +1000

    Don't listen on HTTPS port

--- conanpatcher/conanpatcher-server/Program.cs
+++ conanpatcher/conanpatcher-server/Program.cs
@@ -19,6 +19,7 @@ namespace conanpatcher_server
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5000")
                 .UseStartup<Startup>();
     }
 }
